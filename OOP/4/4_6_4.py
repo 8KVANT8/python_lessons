a467ee4552f4e0d57385edf60fb2e9116d1588e2
BUKOV <vaz21081@ua.fm>
2024-06-02 19:34:33 +0300

    4_6_4  готово

--- OOP/4/4_6_4.py
+++ OOP/4/4_6_4.py
@@ -11,11 +11,13 @@ class Integer(Digit):
         if type(value) != int:
             raise TypeError('значение не соответствует типу объекта')
 
+
 class Float(Digit):
     def __init__(self, value):
         super().__init__(value)
         if type(value) != float:
             raise TypeError('значение не соответствует типу объекта')
+
 
 class Positive(Digit):
     def __init__(self, value):
@@ -23,9 +25,33 @@ class Positive(Digit):
         if value < 0:
             raise TypeError('значение не соответствует типу объекта')
 
+
 class Negative(Digit):
     def __init__(self, value):
         super().__init__(value)
         if value > -1:
             raise TypeError('значение не соответствует типу объекта')
 
+
+class PrimeNumber(Integer, Positive):
+    def __init__(self, v):
+        super().__init__(v)
+
+
+class FloatPositive(Float, Positive):
+    def __init__(self, v):
+        super().__init__(v)
+
+
+digits = [PrimeNumber(3), PrimeNumber(1), PrimeNumber(4), FloatPositive(1.5), FloatPositive(9.2), FloatPositive(6.5),
+          FloatPositive(3.5), FloatPositive(8.9)]
+
+lst_positive = []  ##  все объекты, относящиеся к классу Positive;
+
+lst_float = []  ##все объекты, относящиеся к классу Float.
+
+for X in digits:
+    if isinstance(X, Positive):
+        lst_positive.append(X)
+    elif isinstance(X, Float):
+        lst_float.append(X)
